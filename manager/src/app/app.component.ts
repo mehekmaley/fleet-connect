9d34b55e85e740afee9b7460b7ab577f3ddb5d2a
mehekmaley <mehekmaley@gmail.com>
2021-05-24 19:37:04 +0530

    Update app.component.ts

--- manager/src/app/app.component.ts
+++ manager/src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sideNav = "false"
 
-  onResize(event: any) {
-    this.sideNav = (event.target.innerWidth <= 400) ? "true" : "false";
-  }
   
 }
